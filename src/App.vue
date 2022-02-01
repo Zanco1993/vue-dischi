c71256f606652bddf5ad3e998af9b8cd6d59ddb7
Zanco1993 <zanconatofabio@gmail.com>
2022-02-01 18:54:08 +0100

    BONUS completato + bugfix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <header-app />
-    <search-app v-if="loadingAPI === true" @search="selectGenre"/>
+    <search-app
+      v-if="loadingAPI === true"
+      @searchGenre="selectGenre"
+      @searchArtist="selectArtist"
+    />
     <!-- eseguo l'applicazione solo quando loadingAPI sarà true -->
-    <main-container v-if="loadingAPI" :dischi="dischiGenre" />
+    <main-container v-if="loadingAPI" :dischi="dischiFilter" />
     <loader-app v-else />
   </div>
 </template>
@@ -27,7 +31,7 @@ export default {
   data() {
     return {
       dischi: [],
-      dischiGenre: [],
+      dischiFilter: [],
       loadingAPI: false,
     };
   },
@@ -40,7 +44,7 @@ export default {
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((element) => {
         this.dischi = element.data.response;
-        this.dischiGenre = element.data.response;
+        this.dischiFilter = element.data.response;
 
         setTimeout(() => {
           // assegno il valore di 'success' presente nei dati e lo assegno a loadingAPI
@@ -49,14 +53,28 @@ export default {
       });
   },
   methods: {
-    selectGenre(select) {
-      console.log("prova");
-      this.dischiGenre = this.dischi.filter((genere) => {
-        return genere.genre.includes(select);
+    selectGenre(input) {
+      // console.log("prova");
+      // popolo l'array di dischiFilter con il genere musicale
+      this.dischiFilter = this.dischi.filter((genere) => {
+        return genere.genre.includes(input);
       });
-      if(select === 'All') {
-        this.dischiGenre = this.dischi
+      // se clicco All, l'array torno come quello originale 
+      if (input === "All") {
+        this.dischiFilter = this.dischi;
       }
+      
+    },
+    selectArtist(input) {
+      // console.log("prova");
+      // popolo l'array di dischiFilter con il l'autore
+      this.dischiFilter = this.dischi.filter((artista) => {
+        return artista.author.includes(input);
+      });
+      if (input === "All") {
+        this.dischiFilter = this.dischi;
+      }
+
     },
   },
 };
--- src/components/SearchApp.vue
+++ src/components/SearchApp.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="content-input">
-    <span>Seleziona il genere: </span>
-    <select @change="$emit('search', select)" v-model="select">
-      <option value="All">All</option>
-      <option value="Rock">Rock</option>
-      <option value="Pop">Pop</option>
-      <option value="Jazz">Jazz</option>
-      <option value="Metal">Metal</option>
-    </select>
+  <div>
+    <div class="content-input">
+      <span>Seleziona il genere: </span>
+      <select @change="$emit('searchGenre', input)" v-model="input">
+        <option disabled value="">Seleziona Genere</option>
+        <option value="All">All</option>
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+      </select>
+    </div>
+    <div class="content-input">
+      <span>Seleziona Album: </span>
+      <select @change="$emit('searchArtist', input)" v-model="input">
+        <option disabled value="">Seleziona Artista</option>
+        <option value="All">All</option>
+        <option value="Bon Jovi">Bon Jovi</option>
+        <option value="Queen">Queen</option>
+        <option value="Sting">Sting</option>
+        <option value="Steve Gadd Band">Steve Gadd Band</option>
+        <option value="Iron Maiden">Iron Maiden</option>
+        <option value="Eric Clapton">Eric Clapton</option>
+        <option value="Deep Purple">Deep Purple</option>
+        <option value="Metallica">Metallica</option>
+        <option value="Dave Weckl">Dave Weckl</option>
+        <option value="Michael Jacjson">Michael Jacjson</option>
+      </select>
+    </div>
+
+
   </div>
+  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      select: "",
+      input: ''
     };
   },
 };
@@ -24,9 +47,13 @@ export default {
 <style lang="scss" scoped>
 .content-input {
   text-align: center;
-  padding: 50px;
+  padding: 20px;
 
   span {
+    color: white;
+    font-size: 20px;
+  }
+  div {
     color: white;
     font-size: 20px;
   }
